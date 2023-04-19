6692bcc817c87362dc4ad0f5141b5a1925429aa6
danyela2000 <terraplanet20@gmail.com>
2023-04-19 15:57:24 +0300

    A tweeter clone app
    Twimba - A tweeter clone app

---
+++ index.js
@@ -0,0 +1,193 @@
+import { tweetsData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if(e.target.id === 'tweet-btn'){
+        handleTweetBtnClick()
+        
+    } else if(e.target.dataset.sendReply){ 
+        // I targeted the "SEND" btn by the tweet uuid which is stored in the dataset.sendReply
+        // Call the function using the tweet uuid as an argument
+        addReplyBtnClick(e.target.dataset.sendReply)
+        
+    } else if(e.target.dataset.delTweetBtn){
+        delTweetBtn(e.target.dataset.delTweetBtn)
+    }
+})
+ 
+function handleLikeClick(tweetId){ 
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render() 
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
+
+    if(tweetInput.value){
+        tweetsData.unshift({
+            handle: `@Scrimba`,
+            profilePic: `images/scrimbalogo.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+    render()
+    tweetInput.value = ''
+    }
+
+}
+
+function addReplyBtnClick(tweetId){
+      // I selected the reply textarea by its id (which is the tweet's uuid 
+      //We want to target the textarea and the tweet which it belongs to.
+     const replyInput = document.getElementById(tweetId)
+     console.log(replyInput.value)
+     
+     const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    //for each object I created and added the new reply
+    if(replyInput.value){
+        targetTweetObj.replies.unshift(
+            {
+            handle: `@Scrimba`,
+            profilePic: `images/scrimbalogo.png`,
+            tweetText: replyInput.value,
+        },)
+       render()
+       replyInput.value =''
+    }
+}
+
+
+function delTweetBtn(tweetId){
+   document.getElementById(`tweet-${tweetId}`).classList.add("del-tweet")
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
+    
+    tweetsData.forEach(function(tweet){
+        
+        let likeIconClass = ''
+        
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        
+        let retweetIconClass = ''
+        
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+        
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+            })
+        }
+        
+          
+        feedHtml += `
+<div class="tweet" id='tweet-${tweet.uuid}'>
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>
+        </div> 
+        <p class="del-tweet-btn" data-del-tweet-btn="${tweet.uuid}">✖</p>              
+    </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        <textarea placeholder = 'Add a reply' class="reply-section" id='${tweet.uuid}'
+        data-reply-area="${tweet.uuid}"></textarea>
+       <button class='send-reply-btn' data-send-reply="${tweet.uuid}">Send</button>
+        ${repliesHtml}
+    </div>   
+</div>
+`
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
